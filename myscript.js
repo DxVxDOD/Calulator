96d4fb9db48c776ed07f6239071cbf237d526f57
David Orbang <orbangdavid@yahoo.com>
2022-11-02 13:50:04 +0000

    Restarted the calculator project from scratch original code was sloppy and messy following tutorial for better/cleaner code

--- myscript.js
+++ myscript.js
@@ -1,6 +1,15 @@
-const buttons = Array.from(document.querySelectorAll(".button-item"));
-buttons.forEach(button => button.addEventListener("click", getInfo))
-
-function getInfo (e) {
-    return console.log(e.target.id);
+class Calculator {
+    constructor(previousTextElement, currenTextElement) {
+        this.previousTextElement = previousTextElement;
+        this.currenTextElement = currenTextElement;
+    }
 }
+
+const numberBtn = document.querySelectorAll("[data-number]");
+const operationBtn = document.querySelectorAll("[data-operation]");
+const clearBtn = document.querySelector("[data-clear]");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const previousTextElement = document.querySelector("[data-pervious-operand]");
+const currenTextElement = document.querySelector("[data-current-operand]");
+
